e8c7606cb8f973c3f443535f9c9aa740cc1cb1d5
Kacper Kwasny <kwasny.it@gmail.com>
2022-05-17 21:15:10 +0200

    lab7 wyszukiwanie wzorcow zad0 regex

---
+++ lab7-wyszukiwanie-wzorcow/zad0-regex.py
@@ -0,0 +1,27 @@
+from sys import argv
+import regex as re
+
+def get_content(file_path) -> tuple[str, int]:
+    with open(file_path, "r", encoding="utf-8") as f:
+        content = f.read()
+    # find line length
+    for i, l in enumerate(content):
+        if l == "\n": 
+            return content.replace("\n", ""), i
+
+def find(content, regex_str) -> list[tuple[int, int]]:
+    coords: list[tuple[int, int]] = []
+    pattern = re.compile(regex_str)
+    for match in pattern.finditer(content):
+        yield match.start()
+
+if __name__ == "__main__":
+    content, line_len = get_content(argv[1])
+    restr = f"ABC.{'{'}{line_len-3}{'}'}B.{'{'}{line_len-1}{'}'}C"
+    for start in find(content, restr):
+        print(start//line_len, start%line_len)
+        print(content[start:start+3])
+        print(content[start + line_len])
+        print(content[start + line_len * 2])
+
+
